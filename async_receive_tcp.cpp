b76e2be735b7ca028b5a459a5baea7b72e3e9937
user_ <user_@gmail.com>
2023-12-28 11:32:17 +1100

    all works finally

--- async_receive_tcp.cpp
+++ async_receive_tcp.cpp
@@ -5,18 +5,20 @@
 #include <cstdlib>
 #include <boost/asio.hpp>
 #include <boost/regex.hpp>
-#include <boost/bind.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <boost/asio/posix/basic_descriptor.hpp>
 
 using boost::asio::ip::tcp;
 
 class tcp_client {
     public: 
         tcp_client(boost::asio::io_context& io_context, std::string_view host, std::string_view port) 
-        : _io_context(io_context), _socket(io_context) {
+        : _io_context(io_context), _socket(io_context), _stdin(io_context, ::dup(STDIN_FILENO)) {
             resolve_host(host, port);
             boost::asio::connect(_socket, _endpoints);
-            read_from_host();
-            write_to_host("hello\n");
+            read_from_socket();
+            // write_to_host("hello making sure\n");
+            read_from_stdin();
         }
 
         void resolve_host(std::string_view host, std::string_view port) {
@@ -24,41 +26,58 @@ class tcp_client {
             _endpoints = resolver.resolve(host, port);
         }
 
-        void read_from_host() {
-                boost::asio::async_read_until(_socket, _buffer, '\n', 
-                    std::bind(&tcp_client::handle_read, this,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
+        void read_from_socket() {
+            boost::asio::async_read_until(_socket, _buffer, '\n', 
+                std::bind(&tcp_client::handle_read_socket, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
+
+        void read_from_stdin() {
+            _stdin.async_read_some(boost::asio::buffer(_stdin_buffer, 15), 
+                std::bind(&tcp_client::handle_read_stdin, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
         }
 
         void write_to_host(std::string line) {
-                boost::asio::write(_socket, boost::asio::buffer(line));
+            boost::asio::write(_socket, boost::asio::buffer(line));
         }
         
     private:
         boost::asio::io_context& _io_context;
         tcp::socket _socket;
         tcp::resolver::results_type _endpoints;
-        std::array<char, 128> _buf;
         boost::asio::streambuf _buffer;
+        // boost::asio::streambuf _stdin_buffer;
+        std::array<char, 128> _stdin_buffer;
+        boost::asio::posix::stream_descriptor _stdin;
 
-        void handle_read(const boost::system::error_code& error, std::size_t bytes_transferred) {
+        void handle_read_socket(const boost::system::error_code& error, std::size_t bytes_transferred) {
             std::istream istream(&_buffer);
             std::cout << istream.rdbuf();
 
             _buffer.consume(_buffer.size());
+            read_from_socket();
+        }
 
-            boost::asio::async_read_until(_socket, _buffer, '\n', 
-                    std::bind(&tcp_client::handle_read, this,
-                    boost::asio::placeholders::error,
-                    boost::asio::placeholders::bytes_transferred));
+        void handle_read_stdin(const boost::system::error_code& error, std::size_t bytes_transferred) {
+            // boost::asio::posix::descriptor_base::bytes_readable command(true);
+            // _stdin.io_control(command);
+            // std::size_t bytes_readable = command.get();
+            // std::cout << "bytes ready: " << bytes_readable << std::endl;
+            // std::cout << "bytes: " << bytes_transferred << std::endl;
+            // for (std::size_t i = 0; i < bytes_transferred; i++) {
+            //     std::cout << _stdin_buffer[i];
+            // }
+            std::string data = std::string(_stdin_buffer.data(), bytes_transferred);
+            write_to_host(data);
+            read_from_stdin();
         }
 
         void handle_write(const boost::system::error_code& error, std::size_t bytes_transferred) {
         
         }
-
-        
 };
 
 
@@ -74,10 +93,6 @@ int main(int argc, char* argv[]) {
         tcp_client client(io_context, argv[1], argv[2]);
         io_context.run();
         
-        for (std::string line; std::getline(std::cin, line);) {
-            std::cout << line << std::endl;
-        }
-
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;

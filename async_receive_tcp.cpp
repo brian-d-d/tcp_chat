5e59bcd3ec2a39d98ee520aafbf6b4b15ee74032
user_ <user_@gmail.com>
2023-12-28 11:39:44 +1100

    cleaned up

--- async_receive_tcp.cpp
+++ async_receive_tcp.cpp
@@ -12,12 +12,11 @@ using boost::asio::ip::tcp;
 
 class tcp_client {
     public: 
-        tcp_client(boost::asio::io_context& io_context, std::string_view host, std::string_view port) 
-        : _io_context(io_context), _socket(io_context), _stdin(io_context, ::dup(STDIN_FILENO)) {
+        tcp_client(boost::asio::io_context& io_context, std::string_view host, std::string_view port) :
+        _io_context(io_context), _socket(io_context), _stdin(io_context, ::dup(STDIN_FILENO)) {
             resolve_host(host, port);
             boost::asio::connect(_socket, _endpoints);
             read_from_socket();
-            // write_to_host("hello making sure\n");
             read_from_stdin();
         }
 
@@ -27,14 +26,14 @@ class tcp_client {
         }
 
         void read_from_socket() {
-            boost::asio::async_read_until(_socket, _buffer, '\n', 
+            boost::asio::async_read_until(_socket, _socket_buffer, '\n', 
                 std::bind(&tcp_client::handle_read_socket, this,
                 boost::asio::placeholders::error,
                 boost::asio::placeholders::bytes_transferred));
         }
 
         void read_from_stdin() {
-            _stdin.async_read_some(boost::asio::buffer(_stdin_buffer, 15), 
+            _stdin.async_read_some(boost::asio::buffer(_stdin_buffer, _stdin_buffer.size()), 
                 std::bind(&tcp_client::handle_read_stdin, this,
                 boost::asio::placeholders::error,
                 boost::asio::placeholders::bytes_transferred));
@@ -48,35 +47,22 @@ class tcp_client {
         boost::asio::io_context& _io_context;
         tcp::socket _socket;
         tcp::resolver::results_type _endpoints;
-        boost::asio::streambuf _buffer;
-        // boost::asio::streambuf _stdin_buffer;
+        boost::asio::streambuf _socket_buffer;
         std::array<char, 128> _stdin_buffer;
         boost::asio::posix::stream_descriptor _stdin;
 
         void handle_read_socket(const boost::system::error_code& error, std::size_t bytes_transferred) {
-            std::istream istream(&_buffer);
+            std::istream istream(&_socket_buffer);
             std::cout << istream.rdbuf();
 
-            _buffer.consume(_buffer.size());
+            _socket_buffer.consume(_socket_buffer.size());
             read_from_socket();
         }
 
         void handle_read_stdin(const boost::system::error_code& error, std::size_t bytes_transferred) {
-            // boost::asio::posix::descriptor_base::bytes_readable command(true);
-            // _stdin.io_control(command);
-            // std::size_t bytes_readable = command.get();
-            // std::cout << "bytes ready: " << bytes_readable << std::endl;
-            // std::cout << "bytes: " << bytes_transferred << std::endl;
-            // for (std::size_t i = 0; i < bytes_transferred; i++) {
-            //     std::cout << _stdin_buffer[i];
-            // }
             std::string data = std::string(_stdin_buffer.data(), bytes_transferred);
             write_to_host(data);
             read_from_stdin();
-        }
-
-        void handle_write(const boost::system::error_code& error, std::size_t bytes_transferred) {
-        
         }
 };
 
